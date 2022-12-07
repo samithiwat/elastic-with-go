3190b785c33f4bf5f1cb15d3d610fb432bb41a09
samithiwat <samithiwat2544@gmail.com>
2022-12-07 16:41:51 +0700

    Update index structure

--- src/internal/domain/entity/chula-course/course/course-doc.entity.go
+++ src/internal/domain/entity/chula-course/course/course-doc.entity.go
@@ -7,5 +7,8 @@ type CourseDoc struct {
 	CourseNameEn string  `json:"courseNameEn"`
 	CourseDescTh string  `json:"courseDescTh"`
 	CourseDescEn string  `json:"courseDescEn"`
+	GenEdType    string  `json:"genEdType"`
+	DayOfWeek    string  `json:"dayOfWeek"`
+	PeriodRange  string  `json:"periodRange"`
 	RawData      *Course `json:"rawData"`
 }
