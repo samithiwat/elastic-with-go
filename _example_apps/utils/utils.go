01aa3d31143ff8c9abc5e9cd5e479442b21bd474
samithiwat <samithiwat2544@gmail.com>
2022-12-10 23:07:12 +0700

    Create constant for _example_apps

--- _example_apps/utils/utils.go
+++ _example_apps/utils/utils.go
@@ -1,6 +1,11 @@
 package utils
 
-import "github.com/google/uuid"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/google/uuid"
+)
 
 func StringAdr(in string) *string {
 	return &in
@@ -12,4 +17,19 @@ func BoolAdr(b bool) *bool {
 
 func UUIDAdr(in uuid.UUID) *uuid.UUID {
 	return &in
+}
+
+func AppendDocToBuffer(docId string, docData interface{}, buf *bytes.Buffer) error {
+	meta := []byte(fmt.Sprintf(`{ "index" : { "_id" : "%s" } }%s`, docId, "\n"))
+	data, err := json.Marshal(docData)
+	if err != nil {
+		return err
+	}
+	data = append(data, "\n"...)
+
+	buf.Grow(len(meta) + len(data))
+	buf.Write(meta)
+	buf.Write(data)
+
+	return nil
 }
