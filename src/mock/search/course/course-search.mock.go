544689393d9f466881b7c0e8719cc136efccc75e
samithiwat <samithiwat2544@gmail.com>
2022-12-11 00:46:15 +0700

    Update mock file

--- src/mock/search/course/course-search.mock.go
+++ src/mock/search/course/course-search.mock.go
@@ -9,6 +9,16 @@ type RepositoryMock struct {
 	mock.Mock
 }
 
+func (r *RepositoryMock) Insert(s string, c *course.Course) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (r *RepositoryMock) BulkInsert(i *[]*course.Course) error {
+	//TODO implement me
+	panic("implement me")
+}
+
 func (r *RepositoryMock) Search(queryString string, result *[]*course.Course) error {
 	args := r.Called(queryString, result)
 
