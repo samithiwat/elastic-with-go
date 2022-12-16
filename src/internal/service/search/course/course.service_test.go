5d4ded12474db175badcb7863ae61f024f1ea48a
samithiwat <samithiwat2544@gmail.com>
2022-12-16 14:47:05 +0700

    Update mock and unit test

--- src/internal/service/search/course/course.service_test.go
+++ src/internal/service/search/course/course.service_test.go
@@ -3,6 +3,7 @@ package course
 import (
 	"context"
 	"github.com/go-faker/faker/v4"
+	"github.com/samithiwat/elastic-with-go/src/internal/domain/entity"
 	"github.com/samithiwat/elastic-with-go/src/internal/domain/entity/chula-course/course"
 	courseMock "github.com/samithiwat/elastic-with-go/src/mock/course"
 	courseSearchMock "github.com/samithiwat/elastic-with-go/src/mock/search/course"
@@ -17,6 +18,7 @@ import (
 
 type CourseSearchServiceTest struct {
 	suite.Suite
+	Metadata           *entity.PaginationMetadata
 	Course             *course.Course
 	CourseList         []*course.Course
 	CourseDtoList      []*pb.Course
@@ -30,6 +32,14 @@ func TestCourseSearchService(t *testing.T) {
 }
 
 func (t *CourseSearchServiceTest) SetupTest() {
+	t.Metadata = &entity.PaginationMetadata{
+		ItemsPerPage: rand.Intn(100),
+		ItemCount:    rand.Intn(100),
+		TotalItem:    rand.Intn(10000),
+		CurrentPage:  rand.Intn(99),
+		TotalPage:    rand.Intn(100),
+	}
+
 	t.CourseList = courseMock.CreateCourseList(rand.Intn(5)+1, false)
 	t.Course = t.CourseList[0]
 	t.EmptyCourseList = []*course.Course{}
@@ -42,58 +52,71 @@ func (t *CourseSearchServiceTest) SetupTest() {
 }
 
 func (t *CourseSearchServiceTest) TestSearchSuccess() {
-	query := faker.Word()
+	want := &pb.SearchResponse{Pagination: &pb.CoursePagination{
+		Items: t.CourseDtoList,
+		Meta:  t.Metadata.ToProto(),
+	}}
 
-	want := &pb.SearchResponse{Course: t.CourseDtoList}
+	testSearchSuccess(t.T(), want, &t.CourseList, t.CacheTTL, 1, 20)
+	testSearchSuccess(t.T(), want, &t.CourseList, t.CacheTTL, 10, 20)
+	testSearchSuccess(t.T(), want, &t.CourseList, t.CacheTTL, 50, 10)
+	testSearchSuccess(t.T(), want, &t.CourseList, t.CacheTTL, 100, 10)
 
-	var emptyCourseList []*course.Course
-
-	courseSearchRepo := courseSearchMock.RepositoryMock{}
-	courseSearchRepo.On("Search", &pb.SearchRequest{Keyword: query}, &emptyCourseList).Return(&t.CourseList, nil)
-
-	courseSearchSrv := NewService(&courseSearchRepo, t.CacheTTL)
-
-	actual, err := courseSearchSrv.Search(context.Background(), &pb.SearchRequest{
-		Keyword: query,
-	})
-
-	assert.Nil(t.T(), err)
-	assert.Equal(t.T(), want, actual)
 }
 
-func (t *CourseSearchServiceTest) TestSearchWithFilterSuccess() {
+func testSearchSuccess(t *testing.T, want *pb.SearchResponse, courseList *[]*course.Course, cacheTTL int, page int32, limit int32) {
 	query := faker.Word()
-
-	want := &pb.SearchResponse{Course: t.CourseDtoList}
+	req := &pb.SearchRequest{
+		Keyword: query,
+		PaginationQuery: &pb.PaginationQuery{
+			Limit: limit,
+			Page:  page,
+		},
+	}
 
 	var emptyCourseList []*course.Course
 
 	courseSearchRepo := courseSearchMock.RepositoryMock{}
-	courseSearchRepo.On("Search", &pb.SearchRequest{Keyword: query}, &emptyCourseList).Return(&t.CourseList, nil)
+	courseSearchRepo.On("Search", req, &emptyCourseList, &entity.PaginationMetadata{
+		ItemsPerPage: int(limit),
+		CurrentPage:  int(page),
+	}).Return(courseList, &entity.PaginationMetadata{
+		ItemsPerPage: int(want.Pagination.Meta.ItemsPerPage),
+		ItemCount:    int(want.Pagination.Meta.ItemCount),
+		TotalItem:    int(want.Pagination.Meta.TotalItem),
+		CurrentPage:  int(want.Pagination.Meta.CurrentPage),
+		TotalPage:    int(want.Pagination.Meta.TotalPage),
+	}, nil)
 
-	courseSearchSrv := NewService(&courseSearchRepo, t.CacheTTL)
+	courseSearchSrv := NewService(&courseSearchRepo, cacheTTL)
 
-	actual, err := courseSearchSrv.Search(context.Background(), &pb.SearchRequest{
-		Keyword: query,
-	})
+	actual, err := courseSearchSrv.Search(context.Background(), req)
 
-	assert.Nil(t.T(), err)
-	assert.Equal(t.T(), want, actual)
+	assert.Nil(t, err)
+	assert.Equal(t, want, actual)
 }
 
 func (t *CourseSearchServiceTest) TestSearchElasticsearchConnectionError() {
 	query := faker.Word()
+	req := &pb.SearchRequest{
+		Keyword: query,
+		PaginationQuery: &pb.PaginationQuery{
+			Limit: int32(t.Metadata.ItemsPerPage),
+			Page:  int32(t.Metadata.CurrentPage),
+		},
+	}
 
 	var emptyCourseList []*course.Course
 
 	courseSearchRepo := courseSearchMock.RepositoryMock{}
-	courseSearchRepo.On("Search", &pb.SearchRequest{Keyword: query}, &emptyCourseList).Return(nil, status.Error(codes.Unavailable, "cannot connect to elasticsearch"))
+	courseSearchRepo.On("Search", req, &emptyCourseList, &entity.PaginationMetadata{
+		ItemsPerPage: t.Metadata.ItemsPerPage,
+		CurrentPage:  t.Metadata.CurrentPage,
+	}).Return(nil, nil, status.Error(codes.Unavailable, "cannot connect to elasticsearch"))
 
 	courseSearchSrv := NewService(&courseSearchRepo, t.CacheTTL)
 
-	actual, err := courseSearchSrv.Search(context.Background(), &pb.SearchRequest{
-		Keyword: query,
-	})
+	actual, err := courseSearchSrv.Search(context.Background(), req)
 
 	st, ok := status.FromError(err)
 	assert.True(t.T(), ok)
@@ -103,17 +126,25 @@ func (t *CourseSearchServiceTest) TestSearchElasticsearchConnectionError() {
 
 func (t *CourseSearchServiceTest) TestSearchElasticsearchDecodeError() {
 	query := faker.Word()
+	req := &pb.SearchRequest{
+		Keyword: query,
+		PaginationQuery: &pb.PaginationQuery{
+			Limit: int32(t.Metadata.ItemsPerPage),
+			Page:  int32(t.Metadata.CurrentPage),
+		},
+	}
 
 	var emptyCourseList []*course.Course
 
 	courseSearchRepo := courseSearchMock.RepositoryMock{}
-	courseSearchRepo.On("Search", &pb.SearchRequest{Keyword: query}, &emptyCourseList).Return(nil, status.Error(codes.Internal, "cannot decode to struct"))
+	courseSearchRepo.On("Search", req, &emptyCourseList, &entity.PaginationMetadata{
+		ItemsPerPage: t.Metadata.ItemsPerPage,
+		CurrentPage:  t.Metadata.CurrentPage,
+	}).Return(nil, nil, status.Error(codes.Internal, "cannot decode to struct"))
 
 	courseSearchSrv := NewService(&courseSearchRepo, t.CacheTTL)
 
-	actual, err := courseSearchSrv.Search(context.Background(), &pb.SearchRequest{
-		Keyword: query,
-	})
+	actual, err := courseSearchSrv.Search(context.Background(), req)
 
 	st, ok := status.FromError(err)
 	assert.True(t.T(), ok)
