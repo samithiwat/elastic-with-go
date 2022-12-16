864a0b5a6492dcae9bb09e73d4ba7d0c72fb032b
samithiwat <samithiwat2544@gmail.com>
2022-12-16 14:46:41 +0700

    Update repository interface

--- src/internal/repository/elasticsearch/elasticsearch.repository.go
+++ src/internal/repository/elasticsearch/elasticsearch.repository.go
@@ -3,10 +3,11 @@ package elasticsearch
 import (
 	"bytes"
 	"github.com/elastic/go-elasticsearch/v8/typedapi/core/search"
+	"github.com/samithiwat/elastic-with-go/src/internal/domain/entity"
 )
 
 type Repository interface {
-	Search(string, *search.Request, *map[string]interface{}) error
+	Search(indexName string, req *search.Request, result *map[string]interface{}, meta *entity.PaginationMetadata) error
 	Insert(indexName string, docID string, docData any) error
 	InsertBulk(indexName string, buf *bytes.Buffer) error
 }
