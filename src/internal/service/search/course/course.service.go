952f352cc52e141f56a77742f87893c637661cb9
samithiwat <samithiwat2544@gmail.com>
2022-12-16 17:39:12 +0700

    Implement the service

--- src/internal/service/search/course/course.service.go
+++ src/internal/service/search/course/course.service.go
@@ -3,7 +3,9 @@ package course
 import (
 	"context"
 	"github.com/rs/zerolog/log"
+	courseDto "github.com/samithiwat/elastic-with-go/src/internal/domain/dto/course"
 	"github.com/samithiwat/elastic-with-go/src/internal/domain/entity"
+	commonChulaCourse "github.com/samithiwat/elastic-with-go/src/internal/domain/entity/chula-course/common"
 	"github.com/samithiwat/elastic-with-go/src/internal/domain/entity/chula-course/course"
 	courseRepo "github.com/samithiwat/elastic-with-go/src/internal/repository/elasticsearch/course"
 	"github.com/samithiwat/elastic-with-go/src/pb"
@@ -26,12 +28,10 @@ func (s *service) Search(_ context.Context, req *pb.SearchRequest) (*pb.SearchRe
 		result      []*pb.Course
 		queryResult []*course.Course
 	)
-	meta := &entity.PaginationMetadata{
-		ItemsPerPage: int(req.PaginationQuery.Limit),
-		CurrentPage:  int(req.PaginationQuery.Page),
-	}
 
-	if err := s.courseRepo.Search(req, &queryResult, meta); err != nil {
+	meta, filter := createMetaDataAndFilter(req)
+
+	if err := s.courseRepo.Search(filter, &queryResult, meta); err != nil {
 		log.Error().
 			Err(err).
 			Str("service", "course_search").
@@ -49,4 +49,32 @@ func (s *service) Search(_ context.Context, req *pb.SearchRequest) (*pb.SearchRe
 		Items: result,
 		Meta:  meta.ToProto(),
 	}}, nil
+}
+
+func createMetaDataAndFilter(req *pb.SearchRequest) (*entity.PaginationMetadata, *courseDto.Filter) {
+	meta := &entity.PaginationMetadata{
+		ItemsPerPage: int(req.PaginationQuery.Limit),
+		CurrentPage:  int(req.PaginationQuery.Page),
+	}
+
+	filter := &courseDto.Filter{
+		Keyword: req.Keyword,
+	}
+
+	if req.Filter != nil {
+		filter.DayOfWeeks = req.Filter.DayOfWeek
+		filter.GenEdTypes = req.Filter.GenEdType
+		filter.Semester = req.Filter.Semester
+		filter.AcademicYear = req.Filter.AcademicYear
+		filter.StudyProgram = req.Filter.StudyProgram
+
+		if req.Filter.PeriodRange != nil {
+			filter.PeriodRange = &commonChulaCourse.Period{
+				Start: req.Filter.PeriodRange.Start,
+				End:   req.Filter.PeriodRange.End,
+			}
+		}
+	}
+
+	return meta, filter
 }
