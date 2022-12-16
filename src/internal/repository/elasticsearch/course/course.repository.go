f4771de2ffb917e93f66fb795e4ca1c8c9dd8e07
samithiwat <samithiwat2544@gmail.com>
2022-12-16 17:39:04 +0700

    Update repository interface

--- src/internal/repository/elasticsearch/course/course.repository.go
+++ src/internal/repository/elasticsearch/course/course.repository.go
@@ -1,13 +1,13 @@
 package course
 
 import (
+	courseDto "github.com/samithiwat/elastic-with-go/src/internal/domain/dto/course"
 	"github.com/samithiwat/elastic-with-go/src/internal/domain/entity"
 	"github.com/samithiwat/elastic-with-go/src/internal/domain/entity/chula-course/course"
-	"github.com/samithiwat/elastic-with-go/src/pb"
 )
 
 type Repository interface {
-	Search(req *pb.SearchRequest, result *[]*course.Course, meta *entity.PaginationMetadata) error
+	Search(filter *courseDto.Filter, result *[]*course.Course, meta *entity.PaginationMetadata) error
 	Insert(indexName string, in *course.Course) error
 	BulkInsert(in *[]*course.Course) error
 }
