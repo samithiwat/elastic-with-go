fcf6751ce6bd79f5e0efb465510c2169e630b7e7
samithiwat <samithiwat2544@gmail.com>
2023-01-17 15:16:33 +0700

    Implement method

--- src/internal/repository/elasticsearch/elasticsearch-impl.repository.go
+++ src/internal/repository/elasticsearch/elasticsearch-impl.repository.go
@@ -26,7 +26,7 @@ func NewRepository(esTypedClient *elasticsearch.TypedClient, esDefaultClient *el
 	}
 }
 
-func (r repository) Search(indexName string, req *search.Request, result *map[string]interface{}, meta *entity.PaginationMetadata) error {
+func (r *repository) Search(indexName string, req *search.Request, result *map[string]interface{}, meta *entity.PaginationMetadata) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
@@ -54,7 +54,33 @@ func (r repository) Search(indexName string, req *search.Request, result *map[st
 	return nil
 }
 
-func (r repository) Insert(indexName string, docID string, doc any) error {
+func (r *repository) Suggest(indexName string, req *search.Request, result *map[string]interface{}) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	res, err := r.esTypedClient.Search().
+		Index(indexName).
+		Request(req).
+		Do(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	if res.StatusCode > 200 {
+		// TODO add log error
+		return errors.New("Invalid query")
+	}
+	defer res.Body.Close()
+
+	if err := json.NewDecoder(res.Body).Decode(result); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *repository) Insert(indexName string, docID string, doc any) error {
 	if _, err := r.esDefaultClient.Create(indexName, docID, esutil.NewJSONReader(doc)); err != nil {
 		log.Error().
 			Err(err).
@@ -64,7 +90,7 @@ func (r repository) Insert(indexName string, docID string, doc any) error {
 	return nil
 }
 
-func (r repository) InsertBulk(indexName string, buf *bytes.Buffer) error {
+func (r *repository) InsertBulk(indexName string, buf *bytes.Buffer) error {
 	res, err := r.esDefaultClient.Bulk(bytes.NewReader(buf.Bytes()), r.esDefaultClient.Bulk.WithIndex(indexName))
 	if err != nil {
 		return err
